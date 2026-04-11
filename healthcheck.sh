7c44f277ff496c8c11f45976d228a494e0a77539
Gabriel Chamon <gchamon@tuta.io>
2026-04-11 16:25:30 -0300

    add jellyfin as media option

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env ash
 
+target_container="${TARGET_CONTAINER:-plex}"
+self_container="${SELF_CONTAINER:-${HOSTNAME}}"
+healthcheck_path="${HEALTHCHECK_PATH:-/mnt/zurg/movies}"
+
 while true; do
-    if ! ls /mnt/zurg/movies 2>&1 >/dev/null; then
+    if ! ls "$healthcheck_path" 2>&1 >/dev/null; then
         echo rclone mountpoint seems to be down, restarting...
-        docker container restart plex
-        docker container restart plex-healthcheck
+        docker container restart "$target_container"
+        docker container restart "$self_container"
     else
         echo rclone mountpoint seems to be working for now...
     fi
